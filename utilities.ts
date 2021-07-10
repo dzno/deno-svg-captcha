64aec10b860f2530d7f61716c949752f7f49a54f
Moncef Gaha <moncefplastin07@gmail.com>
2021-07-10 15:36:19 +0100

    feat: add text  hashing function

--- utilities.ts
+++ utilities.ts
@@ -1,6 +1,11 @@
+import { Hash } from "./deps.ts";
 export const randomNumber = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
 };
 export const randomHexColor = (): string => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+export const textHash = (text: string) => {
+  return new Hash("md5").digestString(text).hex();
 };
