94ab4a5996d708b1aaed816864ae15442d499a23
Moncef Gaha <moncefplastin07@gmail.com>
2021-07-11 08:42:42 +0100

    feat: add isValid function

--- validate.js
+++ validate.js
@@ -1,1 +1,16 @@
-export {isValid} from "./validate.ts"
+function cookieParser(cookie){
+    const cookiePrams = cookie.split(';').map((e)=> e.trim().split('='))
+    return Object.fromEntries(cookiePrams);
+}
+function isValid(text) {
+    if (text.length<0) {
+        return false
+    }
+
+    if(!(cookieParser(document.cookie)["svg_captcha_text"])){
+        alert("svg_captcha_text cookie not found")
+        return false
+    }
+
+    return cookieParser(document.cookie)["svg_captcha_text"] === md5(text)
+}
